5f743ac0c961f20894957d6d88645eae6c3aa7c4
Jay T <jaytula@gmail.com>
2020-11-18 08:35:40 -0800

    Add useInterval hook

--- src/index.tsx
+++ src/index.tsx
@@ -1,4 +1,4 @@
-import * as React from 'react'
+import React, { useEffect } from 'react'
 import styles from './styles.module.css'
 
 interface Props {
@@ -7,4 +7,13 @@ interface Props {
 
 export const ExampleComponent = ({ text }: Props) => {
   return <div className={styles.test}>Example Component: {text}</div>
+}
+
+export const useInterval = (callback: Function, timeout: number = 1000) => {
+  useEffect(() => {
+    const interval = setInterval(callback, timeout)
+    return () => {
+      clearInterval(interval)
+    }
+  })
 }
